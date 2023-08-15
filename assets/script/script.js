2095028eddd62c13b3683ad7d374c47e5cb7f709
sefufim <132181028+sefufim@users.noreply.github.com>
2023-08-14 22:29:12 -0700

    made the questions for the game

---
+++ assets/script/script.js
@@ -0,0 +1,60 @@
+// questions that will be asked in the game
+let question = [
+  {
+    question:
+      "Which of the following programming languages is used to create the structure and content of a webpage?",
+    choiceA: "",
+    choiceB: "",
+    choiceC: "",
+    choiceD: "",
+  },
+  {
+    question:
+      " Which language is primarily responsible for controlling the behavior and interactivity of a webpage?",
+    choiceA: "",
+    choiceB: "",
+    choiceC: "",
+    choiceD: "",
+  },
+
+  {
+    question: "What is the primary purpose of CSS in web development?",
+    choiceA: "",
+    choiceB: "",
+    choiceC: "",
+    choiceD: "",
+  },
+
+  {
+    question:
+      "Which of the following HTML tags is used to link an external JavaScript file to an HTML document?",
+    choiceA: "",
+    choiceB: "",
+    choiceC: "",
+    choiceD: "",
+  },
+
+  {
+    question:
+      "Which CSS property is used to change the background color of an element?",
+    choiceA: "",
+    choiceB: "",
+    choiceC: "",
+    choiceD: "",
+  },
+  {
+    question:
+      "In JavaScript, which function is used to change the content of an HTML element?",
+    choiceA: "",
+    choiceB: "",
+    choiceC: "",
+    choiceD: "",
+  },
+  {
+    question: "Which HTML tag is used to create a numbered list?",
+    choiceA: "",
+    choiceB: "",
+    choiceC: "",
+    choiceD: "",
+  },
+];
