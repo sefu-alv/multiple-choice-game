022a8dd2a89fa17b911f319f568ba63a6607f1c9
sefufim <132181028+sefufim@users.noreply.github.com>
2023-08-15 18:22:28 -0700

    console logged the games questions and changed a few variable names

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,60 +1,91 @@
 // questions that will be asked in the game
-let question = [
+let askingQuestions = [
   {
     question:
       "Which of the following programming languages is used to create the structure and content of a webpage?",
-    choiceA: "",
-    choiceB: "",
-    choiceC: "",
-    choiceD: "",
+    choiceA: "CSS",
+    choiceB: "HTML",
+    choiceC: "JAVASCRIPT",
+    choiceD: "C++",
+    
+    answer:"b"
   },
   {
     question:
       " Which language is primarily responsible for controlling the behavior and interactivity of a webpage?",
-    choiceA: "",
-    choiceB: "",
-    choiceC: "",
-    choiceD: "",
+    choiceA: "HTML",
+    choiceB: "JAVASCRIPT",
+    choiceC: "CSS",
+    choiceD: "PHP",
+
+    answer:"b"
   },
 
   {
     question: "What is the primary purpose of CSS in web development?",
-    choiceA: "",
-    choiceB: "",
-    choiceC: "",
-    choiceD: "",
+    choiceA: "MAKE WEBSITES RESPONSIVE",
+    choiceB: "CREATE SERVERS",
+    choiceC: "MANIPULATE DATA",
+    choiceD: "STYLE WEBSITES",
+
+    answer:"d"
   },
 
   {
     question:
       "Which of the following HTML tags is used to link an external JavaScript file to an HTML document?",
-    choiceA: "",
-    choiceB: "",
-    choiceC: "",
-    choiceD: "",
+    choiceA: "<link>",
+    choiceB: "<script>",
+    choiceC: "<javascript>",
+    choiceD: "<style>",
+
+    answer:"b"
   },
 
   {
     question:
       "Which CSS property is used to change the background color of an element?",
-    choiceA: "",
-    choiceB: "",
-    choiceC: "",
-    choiceD: "",
+    choiceA: "color",
+    choiceB: "bgcolor",
+    choiceC: "bg-color",
+    choiceD: "background-color",
+
+    answer:"d"
   },
   {
     question:
       "In JavaScript, which function is used to change the content of an HTML element?",
-    choiceA: "",
-    choiceB: "",
-    choiceC: "",
-    choiceD: "",
+    choiceA: "appendElement()",
+    choiceB: "innerHTML()",
+    choiceC: "$()",
+    choiceD: "changeAttribute()",
+
+    answer:"b"
   },
   {
     question: "Which HTML tag is used to create a numbered list?",
-    choiceA: "",
-    choiceB: "",
-    choiceC: "",
-    choiceD: "",
+    choiceA: "<li>",
+    choiceB: "<ul>",
+    choiceC: "<ol>",
+    choiceD: "<p>",
+
+    answer:"c"
   },
 ];
+
+function askQuestion(questions){
+   var questionElement = document.getElementById("q");
+   var optionsElement = document.getElementById('options');
+
+   for(var i = 0; i <questions.length; i++){
+   var currentQuestion = questions[i];
+    console.log(currentQuestion.question);
+    console.log(currentQuestion.choiceA);
+    console.log(currentQuestion.choiceB);
+    console.log(currentQuestion.choiceC);
+    console.log(currentQuestion.choiceD);
+   }
+
+
+}
+askQuestion(askingQuestions);
