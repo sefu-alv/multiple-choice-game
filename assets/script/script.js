8c7ebdc8541b79589b00cbbb362d818c7b2616e9
sefufim <132181028+sefufim@users.noreply.github.com>
2023-08-15 23:41:31 -0700

    got the questions and options to display

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,34 +1,34 @@
 // questions that will be asked in the game
-let askingQuestions = [
+const askingQuestions = [
   {
     question:
       "Which of the following programming languages is used to create the structure and content of a webpage?",
-    choiceA: "CSS",
-    choiceB: "HTML",
-    choiceC: "JAVASCRIPT",
-    choiceD: "C++",
-    
-    answer:"b"
+    A: "CSS",
+    B: "HTML",
+    C: "JAVASCRIPT",
+    D: "C++",
+
+    answer: "b",
   },
   {
     question:
       " Which language is primarily responsible for controlling the behavior and interactivity of a webpage?",
-    choiceA: "HTML",
-    choiceB: "JAVASCRIPT",
-    choiceC: "CSS",
-    choiceD: "PHP",
+    A: "HTML",
+    B: "JAVASCRIPT",
+    C: "CSS",
+    D: "PHP",
 
-    answer:"b"
+    answer: "b",
   },
 
   {
     question: "What is the primary purpose of CSS in web development?",
-    choiceA: "MAKE WEBSITES RESPONSIVE",
-    choiceB: "CREATE SERVERS",
-    choiceC: "MANIPULATE DATA",
-    choiceD: "STYLE WEBSITES",
+    A: "MAKE WEBSITES RESPONSIVE",
+    B: "CREATE SERVERS",
+    C: "MANIPULATE DATA",
+    D: "STYLE WEBSITES",
 
-    answer:"d"
+    answer: "d",
   },
 
   {
@@ -39,53 +39,58 @@ let askingQuestions = [
     choiceC: "<javascript>",
     choiceD: "<style>",
 
-    answer:"b"
+    answer: "b",
   },
 
   {
     question:
       "Which CSS property is used to change the background color of an element?",
-    choiceA: "color",
-    choiceB: "bgcolor",
-    choiceC: "bg-color",
-    choiceD: "background-color",
+    A: "color",
+    B: "bgcolor",
+    C: "bg-color",
+    D:"background-color",
 
-    answer:"d"
+    answer: "d",
   },
   {
     question:
       "In JavaScript, which function is used to change the content of an HTML element?",
-    choiceA: "appendElement()",
-    choiceB: "innerHTML()",
-    choiceC: "$()",
-    choiceD: "changeAttribute()",
+    A: "appendElement()",
+    B: "innerHTML()",
+    C: "$()",
+    D: "changeAttribute()",
 
-    answer:"b"
+    answer: "b",
   },
   {
     question: "Which HTML tag is used to create a numbered list?",
-    choiceA: "<li>",
-    choiceB: "<ul>",
-    choiceC: "<ol>",
-    choiceD: "<p>",
+    A: "<li>",
+    B: "<ul>",
+    C: "<ol>",
+    D: "<p>",
 
-    answer:"c"
+    answer: "c",
   },
 ];
-
-function askQuestion(questions){
-   var questionElement = document.getElementById("q");
-   var optionsElement = document.getElementById('options');
-
-   for(var i = 0; i <questions.length; i++){
-   var currentQuestion = questions[i];
-    console.log(currentQuestion.question);
-    console.log(currentQuestion.choiceA);
-    console.log(currentQuestion.choiceB);
-    console.log(currentQuestion.choiceC);
-    console.log(currentQuestion.choiceD);
-   }
-
+var questionElement = document.getElementById("q");
+var optionsElement = document.getElementById("options");
+var aOption = document.getElementById("a-option");
+var bOption = document.getElementById("b-option");
+var cOption = document.getElementById("c-option");
+var dOption = document.getElementById("d-option");
+//setting index set to -1 so no questions are displayed
+var questionIndex = 0;
+function askQuestion() {
+  //    pushing up the index by one
+  questionIndex++;
+//   pulls up the most current question 
+  var currentQuestion = askingQuestions[questionIndex];
+//   displays current question
+  questionElement.innerText = currentQuestion.question;
+  aOption.innerText=currentQuestion.A;
+  bOption.innerText=currentQuestion.B;
+  cOption.innerText=currentQuestion.C;
+  dOption.innerText=currentQuestion.D;
 
 }
-askQuestion(askingQuestions);
+askQuestion();
