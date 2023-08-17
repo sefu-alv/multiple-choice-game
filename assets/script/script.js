0e90a08ca80b8637c15145153606791109778b1b
sefufim <132181028+sefufim@users.noreply.github.com>
2023-08-17 01:00:51 -0700

    added logic to make the intro text be typed out and then hide once the begin button is hit

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,35 @@
+// start screen
+var start = document.getElementById("start-btn");
+var mainScreen = document.getElementById("main-screen");
+// intro to the quiz
+var textEl = document.getElementById("intro-text");
+var text =
+  "Welcome to the Quiz Realm! Test your coding knowledge as you tackle a series of questions. Each question offers multiple choices: A, B, C, or D. Choose wisely, as wrong answers chip away at your timer. Can you beat the clock and stay in the game? Step up and prove your knowledge skills in the Quiz Realm!";
+var index = 0;
+// checks if words are still being typed
+var stilltyping = true;
+// this function types out the text at a certain rate
+function typingText() {
+  if (index < text.length) {
+    textEl.textContent += text.charAt(index);
+    index++;
+    // sets typing speed
+    setTimeout(typingText, 50);
+  } else {
+    // since still typing is now false it will allow the start button to work
+    stilltyping = false;
+  }
+}
+typingText();
+// start game fuctions
+start.addEventListener("click", function () {
+  if (!stilltyping) {
+    mainScreen.style.display = "block";
+    start.style.display = "none";
+    textEl.style.display = "none";
+  }
+});
+
 // questions that will be asked in the game
 const askingQuestions = [
   {
@@ -48,7 +80,7 @@ const askingQuestions = [
     A: "color",
     B: "bgcolor",
     C: "bg-color",
-    D:"background-color",
+    D: "background-color",
 
     answer: "d",
   },
@@ -83,14 +115,13 @@ var questionIndex = 0;
 function askQuestion() {
   //    pushing up the index by one
   questionIndex++;
-//   pulls up the most current question 
+  //   pulls up the most current question
   var currentQuestion = askingQuestions[questionIndex];
-//   displays current question
+  //   displays current question
   questionElement.innerText = currentQuestion.question;
-  aOption.innerText=currentQuestion.A;
-  bOption.innerText=currentQuestion.B;
-  cOption.innerText=currentQuestion.C;
-  dOption.innerText=currentQuestion.D;
-
+  aOption.innerText = currentQuestion.A;
+  bOption.innerText = currentQuestion.B;
+  cOption.innerText = currentQuestion.C;
+  dOption.innerText = currentQuestion.D;
 }
 askQuestion();
